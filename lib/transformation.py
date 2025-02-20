0caddcea1d6081da11133fafedfbcc47ccbd3939
hamzaiftikhar098 <hamzaiftikhar098@gmail.com>
2025-02-20 18:23:39 +0500

    This is where i added a lopp in an existing file and added a new transformations file

--- lib/transformation.py
+++ lib/transformation.py
@@ -1,1 +1,6 @@
-# this file will contain the tranformations related to the dataframes which we wioll be calling from the main.py
+# this file will contain the tranformations related to the dataframes which we will be calling from the main.py
+list=[0,1,2,3]
+sum=0
+for i in list:
+    sum=sum+i
+    print(sum)
